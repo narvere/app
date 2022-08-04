8b5b5284249d04753b2fd4897d2f92a0122d9fdb
narvere <deniss.hohlov@gmail.com>
2022-08-04 11:53:58 +0300

    Initial commit

---
+++ app/create_app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+# set FLASK_APP=create_app.py
+# flask run
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        return "Hello world123!"
+
+    return app
